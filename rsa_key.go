766a20b45361e48233518313e02257f211c8437b
smartwalle <smartwalle@gmail.com>
2019-08-29 11:11:24 +0800

    fmt

--- rsa_key.go
+++ rsa_key.go
@@ -2,6 +2,10 @@ package crypto4go
 
 import (
 	"bytes"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
 	"strings"
 )
 
@@ -62,4 +66,59 @@ func formatKey(raw, prefix, suffix string) []byte {
 	}
 	buf.WriteString(suffix)
 	return buf.Bytes()
+}
+
+func ParsePKCS1PrivateKey(data []byte) (key *rsa.PrivateKey, err error) {
+	var block *pem.Block
+	block, _ = pem.Decode(data)
+	if block == nil {
+		return nil, errors.New("private key error")
+	}
+
+	key, err = x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return key, err
+}
+
+func ParsePKCS8PrivateKey(data []byte) (key *rsa.PrivateKey, err error) {
+	var block *pem.Block
+	block, _ = pem.Decode(data)
+	if block == nil {
+		return nil, errors.New("private key error")
+	}
+
+	rawKey, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	key, ok := rawKey.(*rsa.PrivateKey)
+	if ok == false {
+		return nil, errors.New("private key error")
+	}
+
+	return key, err
+}
+
+func ParsePublicKey(data []byte) (key *rsa.PublicKey, err error) {
+	var block *pem.Block
+	block, _ = pem.Decode(data)
+	if block == nil {
+		return nil, errors.New("public key error")
+	}
+
+	var pubInterface interface{}
+	pubInterface, err = x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	key, ok := pubInterface.(*rsa.PublicKey)
+	if !ok {
+		return nil, errors.New("public key error")
+	}
+
+	return key, err
 }
